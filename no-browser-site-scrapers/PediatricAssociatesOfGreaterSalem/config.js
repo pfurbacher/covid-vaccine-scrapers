2f561ac2b9469758945a214139a01cee33ae03ca
Paul Furbacher <pfurbacher@mac.com>
2021-05-24 22:54:18 -0400

    Rmoved Highland Ave location

--- no-browser-site-scrapers/PediatricAssociatesOfGreaterSalem/config.js
+++ no-browser-site-scrapers/PediatricAssociatesOfGreaterSalem/config.js
@@ -5,12 +5,12 @@ const site = {
         "https://framework-backend.scheduling.athena.io/u?locationId=2804-102&practitionerId=&contextId=2804",
     graphQLUrl: "https://framework-backend.scheduling.athena.io/v1/graphql",
     locations: [
-        {
-            street: "84 Highland Ave",
-            city: "Salem",
-            state: "MA",
-            zip: "01970",
-        },
+        // {
+        //     street: "84 Highland Ave",
+        //     city: "Salem",
+        //     state: "MA",
+        //     zip: "01970",
+        // },
         {
             street: "30 Tozer Rd",
             city: "Beverly",
